7477f43a18ee85119c51a53a2ccf75f659d81be4
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2023-11-09 13:21:00 -0800

    add waves package

---
+++ waves/__init__.py
@@ -0,0 +1,8 @@
+"""WHaLE initialization."""
+
+import importlib.metadata
+
+from waves.project import Project
+
+
+__version__ = importlib.metadata.version("waves")
---
+++ waves/utilities/__init__.py
@@ -0,0 +1,9 @@
+"""Provides a series of utilities methods that can be removed from the main `project` module."""
+
+from waves.utilities.library import load_yaml, resolve_path
+from waves.utilities.floris_runners import (
+    check_monthly_wind_rose,
+    create_monthly_wind_rose,
+    run_parallel_time_series_floris,
+    calculate_monthly_wind_rose_results,
+)
